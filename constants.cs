261af348238365f4cce9b375d186769a75c6c067
reteshg <reteshgondal@gmail.com>
2019-03-03 23:00:51 -0500

    remove api key from constants

--- constants.cs
+++ constants.cs
@@ -3,6 +3,6 @@ public static class Constants
     public const string datesFolderName = "dates"; //folder name to save text files with dates
     public const string imagesFolderName = "images"; // folder name to save downloaded images
     public const string jsonFolderName = "json"; //folder name to save json files to render webpage
-    public const string NASAApiKey = "MS956pjwjdbPUWeWlxd2idUZHQpFpXOAkh3wPt8p"; //NASA API Key
+    public const string NASAApiKey = ""; //NASA API Key
     public const string NASADataUrl="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="; //NASA URL to get images url
 }
